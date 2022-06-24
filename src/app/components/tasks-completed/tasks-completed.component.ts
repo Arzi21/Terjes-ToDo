4db829e846eefda1bc4737a5da9e8848d4b3a6de
Arzi21 <aretlegland@gmail.com>
2022-06-24 13:57:44 +0200

    began menu switching

--- src/app/components/tasks-completed/tasks-completed.component.ts
+++ src/app/components/tasks-completed/tasks-completed.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Renderer2 } from '@angular/core';
+
 import { TaskInterface } from 'src/app/interfaces/task-interface';
 
 @Component({
@@ -10,15 +11,35 @@ export class TasksCompletedComponent implements OnInit {
 
   @Input() completedTasks: TaskInterface[]|undefined
   @Input() deletedTasks: TaskInterface[]|undefined
+
+  currentListTitle = "Completed";
   
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2,
+  ) { }
 
   ngOnInit(): void {
   }
 
   restoreTask(task:TaskInterface) {
     console.log("Task Restoration Initiated")
+  }
+
+  changeListView(completedTasksList: any, deletedTasksList: any, totalViewControl:any) {
+    console.log("List View Change Initiated")
+    if (this.currentListTitle === "Deleted") {
+      this.currentListTitle = "Completed";
+      this.renderer.setStyle(completedTasksList, 'display', 'block');
+      this.renderer.setStyle(totalViewControl, 'grid-template-rows', '70px 1fr 70px')
+      this.renderer.setStyle(deletedTasksList, 'display', 'none')
+
+    } else {
+      this.currentListTitle = "Deleted";
+      this.renderer.setStyle(completedTasksList, 'display', 'none');
+      this.renderer.setStyle(totalViewControl, 'grid-template-rows', '70px 70px 1fr')
+      this.renderer.setStyle(deletedTasksList, 'display', 'block')
+    }
   }
 
 }
